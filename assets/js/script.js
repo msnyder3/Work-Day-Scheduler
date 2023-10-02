a468a4ad0609b72de3a4a3e7749f39ec831f37eb
Molly Snyder <131561844+msnyder3@users.noreply.github.com>
2023-10-02 14:38:47 -0400

    added looping function to assign p/p/f classes to hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+// Use day.js to grab the date and time info.
+var today = dayjs();
+
+// Use current date and time as text for <p> element with id of "currentDay"
+$('#currentDay').text(today.format('MMMM D, YYYY  h:mm a')); // Formats date and time from dayjs
+
 // saveBtn click event listener
 $('.saveBtn').on('click', function () {
   // get nearby values of the description in jQuery
@@ -20,18 +26,31 @@ $('#hour15 .description').val(localStorage.getItem('hour15'));
 $('#hour16 .description').val(localStorage.getItem('hour16'));
 $('#hour17 .description').val(localStorage.getItem('hour17'));
 
-$(function () {
- 
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+// This function tracks the time and color codes the rows as past, present or future
+function trackEvent() {
+  // get current hour
+  var currentHour = today.hour();
+
+  // loop over each time block
+  $('.time-block').each(function () {
+    var timeBlock = parseInt($(this).attr('id').split("hour")[1]);
+
+    // if the time Id attribute is before the current hour, add the past class
+    if (timeBlock < currentHour) {
+      $(this).addClass('past');
+    } // if the time Id attribute is equal to the current hour, remove the past and future classes and add the present class
+    else if (timeBlock === currentHour) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    } // If the time Id attribute is greater than the current time, remove the past and present classes and add the future class
+    else {
+      $(this).removeClass('past');
+      $(this).removeClass('present');
+      $(this).addClass('future');
+    }
+  });
+}
+
+// call the trackEvent function
+trackEvent();
