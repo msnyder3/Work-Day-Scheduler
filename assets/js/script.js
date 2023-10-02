63c8ecb1833772a307b237533d6f76bf3d123469
Molly Snyder <131561844+msnyder3@users.noreply.github.com>
2023-10-02 14:29:04 -0400

    added in div elements for each hour and added in local storage functionality for each element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,27 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// saveBtn click event listener
+$('.saveBtn').on('click', function () {
+  // get nearby values of the description in jQuery
+  var inputText = $(this).siblings('.description').val();
+  // get the id attribute of the parent div element
+  var hourBlock = $(this).parent().attr('id');
+
+  // save in local storage
+  localStorage.setItem(hourBlock, inputText);
+});
+
+// Pull calendar items from local storage
+$('#hour9 .description').val(localStorage.getItem('hour9'));
+$('#hour10 .description').val(localStorage.getItem('hour10'));
+$('#hour11 .description').val(localStorage.getItem('hour11'));
+$('#hour12 .description').val(localStorage.getItem('hour12'));
+$('#hour13 .description').val(localStorage.getItem('hour13'));
+$('#hour14 .description').val(localStorage.getItem('hour14'));
+$('#hour15 .description').val(localStorage.getItem('hour15'));
+$('#hour16 .description').val(localStorage.getItem('hour16'));
+$('#hour17 .description').val(localStorage.getItem('hour17'));
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
+ 
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
